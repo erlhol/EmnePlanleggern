6c1a152713b5a2464e656512e01f42c0dd0c3b75
Erling Holte <erling.holte@outlook.com>
2023-07-13 19:59:23 +0200

    Add calendar and API-test

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Et prosjekt som skal gjøre det enklere for studenter å planlegge semesteret.
 Her skal du få opp emnene du kan velge i et gitt semester, for å unngå at
 noe krasjer. 
 
-Det skal også være mulig å skrive anmeldelser om emnene. Basert på 
+Det skal også være mulig å skrive anmeldelser om emnene. Basert på anmeldelsene skal du kunne rangere emnene
--- javascripts/main.js
+++ javascripts/main.js
@@ -3,3 +3,24 @@ console.log('This would be the main JS file.');
 function myFunction() {
     document.getElementById("demo").innerHTML = "Teksten ble endret";
   }
+
+  document.getElementById("mon-8").innerHTML = "Meeting";
+  document.getElementById("tue-10").innerHTML = "Lunch";
+  document.getElementById("wed-14").innerHTML = "Training";
+  document.getElementById("thu-16").innerHTML = "Presentation";
+
+function apiTest() {
+    fetch('https://api.nilu.no/stats/day/2018-09-17/2018-09-18/manglerud?components=pm10', {
+        method: 'GET'
+    })
+  .then(response => response.json())
+  .then(data => {
+    // Process the data returned from the API
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  });
+    
+}
