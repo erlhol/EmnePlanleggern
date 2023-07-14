811babbade96ad3827124eb8e54520f8bcaac796
Erling Holte <erling.holte@outlook.com>
2023-07-14 13:32:37 +0200

    Test with objects

--- javascripts/objects.js
+++ javascripts/objects.js
@@ -7,6 +7,10 @@ class Emne {
     introduce() {
       console.log(`The subject ${this.code} has the following description: ${this.description}.`);
     }
+
+    toString() {
+        return `Emne { code: ${this.code}, description: ${this.description} }`;
+    }
   }
   
 const emne = new Emne("IN1000","Introduksjon til objektorientert programmering");
@@ -22,8 +26,17 @@ console.log(emne2.code);
 console.log(emne2.description); 
 emne2.introduce();
 
-// Populate table:
-var numItems = parseInt(prompt("Enter the number of items:"));
+function create_dummy_subjects() {
+    var array = [];
+    for (var i = 0; i <= 51; i++) {
+        array.push(new Emne(i,"emne nummer "+i));
+    }
+    return array;
+}
+
+var subjects = create_dummy_subjects();
+
+var numItems = subjects.length;
 var listContainer = document.getElementById("list-container");
 var paginationContainer = document.getElementById("pagination");
 
@@ -31,17 +44,17 @@ var itemsPerPage = 10; // Number of items to display per page
 var totalPages = Math.ceil(numItems / itemsPerPage);
 var currentPage = 1;
 
-function generateListItems(start, end) {
+function generateListItems(start, end, subjects) {
   listContainer.innerHTML = ""; // Clear the existing list items
 
   for (var i = start; i <= end; i++) {
     var listItem = document.createElement("li");
-    listItem.innerText = "Item " + i;
+    listItem.innerText = subjects[i].toString();
     listContainer.appendChild(listItem);
   }
 }
 
-function updatePaginationButtons() {
+function updatePaginationButtons(subjects) {
   paginationContainer.innerHTML = ""; // Clear the pagination buttons
 
   for (var i = 1; i <= totalPages; i++) {
@@ -53,14 +66,14 @@ function updatePaginationButtons() {
       currentPage = parseInt(this.innerText.split(" ")[1]);
       generateListItems(
         (currentPage - 1) * itemsPerPage + 1,
-        Math.min(currentPage * itemsPerPage, numItems)
+        Math.min(currentPage * itemsPerPage, numItems), subjects
       );
-      updatePaginationButtons();
+      updatePaginationButtons(subjects);
     });
 
     paginationContainer.appendChild(button);
   }
 }
 
-generateListItems(1, itemsPerPage); // Generate initial list
-updatePaginationButtons(); // Generate initial pagination buttons
+generateListItems(0, itemsPerPage,subjects); // Generate initial list
+updatePaginationButtons(subjects); // Generate initial pagination buttons
