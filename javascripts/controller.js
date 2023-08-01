516fea35de303f79f95c4bcffd8ffb7fce5d9389
Erling Holte <erling.holte@outlook.com>
2023-08-01 21:05:36 +0200

    Add await to wait on datasource

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -2,36 +2,35 @@ export default class CourseController {
     constructor(model, view) {
         this.model = model;
         this.view = view;
-        this.view.renderAllCourses(this.model.allCourses);
+        this.init();
+    }
+
+    async init() {
+        await this.view.renderAllCourses(this.model.allCourses);
         this.view.renderSetTotalCredits(this.model.total_credits);
-
-        const courseArray = this.view.courseContainer.childNodes;
-        courseArray.forEach((courseDiv, i) => {
-            const courseOject = this.model.allCourses[i];
-            courseDiv.addEventListener("click", function() {
+        const allCourses = await this.model.allCourses;
+        
+        Array.from(this.view.courseContainer.children).forEach((courseDiv, i) => {
+            const courseOject = allCourses[i];
+            courseDiv.addEventListener("click", () => {
                 if (courseDiv.classList.contains("chosen")) {
-                    this.view.renderNotChosen(courseDiv);
-                    
                     this.model.removeItemFromSelected();
-                    this.model.incrementTotalCredits(courseOject); // but with - as parameter (decrement)
+                    this.model.decrementTotalCredits(courseOject);
 
+                    this.view.renderNotChosen(courseDiv);
                     this.view.renderRemoveFromSelectedCourses(courseOject);
                     this.view.renderRemoveActivites(courseOject);
-                    
-                    this.view.renderSetTotalCourses(this.model.total_credits);
-                  } else {
-                    this.view.renderChosen(courseDiv);
-                    
+                    this.view.renderSetTotalCredits(this.model.total_credits);
+                } else {
                     this.model.addToSelected(courseOject);
-                    this.model.decrementTotalCredits(courseOject);
+                    this.model.incrementTotalCredits(courseOject); 
                     
+                    this.view.renderChosen(courseDiv);
                     this.view.renderSelectedCourse(courseOject);
                     this.view.renderAddCourseActivites(courseOject);
-
-                    this.view.renderSetTotalCourses(this.model.total_credits);
-                  }
-                });
-        })
+                    this.view.renderSetTotalCredits(this.model.total_credits);
+                }
+            });
+        });
     } 
 }
-  
