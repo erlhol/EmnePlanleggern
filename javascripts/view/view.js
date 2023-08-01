cf43d1bd354cf23b1ca57a7bff01fde5d49cb6ce
Erling Holte <erling.holte@outlook.com>
2023-08-01 10:51:09 +0200

    Fix spacing

--- javascripts/view/view.js
+++ javascripts/view/view.js
@@ -1,14 +1,14 @@
 export default class CourseView {
     constructor() {
-      this.app = document.getElementById('app');
+        this.app = document.getElementById('app');
     }
   
     render(data) {
-      this.app.innerHTML = '';
-      data.forEach((item, index) => {
-        const itemElement = document.createElement('div');
-        itemElement.innerHTML = `${index + 1}. ${item}`;
-        this.app.appendChild(itemElement);
-      });
+        this.app.innerHTML = '';
+        data.forEach((item, index) => {
+            const itemElement = document.createElement('div');
+            itemElement.innerHTML = `${index + 1}. ${item}`;
+            this.app.appendChild(itemElement);
+        });
     }
   }
