1593974fef06a9bafa92c63204c649bbd0dc90a3
Erling Holte <erling.holte@outlook.com>
2023-09-09 09:34:36 +0200

    Test edit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is to learn react!
         </a>
       </header>
     </div>
