7640ca2817efa06031f49a5019410805f0bfd590
Erling Holte <erling.holte@outlook.com>
2023-09-10 11:51:12 +0200

    Update json-files with websrcraping content

--- my-app/src/Schedule.js
+++ my-app/src/Schedule.js
@@ -1,7 +1,25 @@
-function Schedule() {
-    return <h1>Schedule</h1>
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
+function Schedule() {
+    // Setup the localizer by providing the moment (or globalize, or Luxon) Object
+    // to the correct localizer.
+    const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+    const MyCalendar = (props) => (
+    <div className="myCustomHeight">
+        <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        />
+    </div>
+    )
+
+    return <h1>Schedule</h1>
 }
+
 export default Schedule;
 
 // TODO: create a smart schedule where
