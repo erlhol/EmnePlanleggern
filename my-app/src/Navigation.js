0eda16ba39af898bb6c8a5eaa0d9d1561b40a097
Erling Holte <erling.holte@outlook.com>
2023-10-07 13:04:18 +0200

    Add icons to Navigation

--- my-app/src/Navigation.js
+++ my-app/src/Navigation.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React from 'react';
+import calendar_icon from './icons/calendar-clock.png';
+import learning_icon from './icons/learning.png';
+
+function ImageWithStyle({ pageHandler, icon, isActive, name }) {
+  const imageStyle = {
+    maxWidth: '60px',
+    maxHeight: '60px',
+    width: 'auto',
+    height: 'auto',
+  };
+
+  return (
+    <div style={{backgroundColor: isActive ? 'lightblue' : 'transparent'}}>
+      <img
+      onClick={pageHandler}
+      style={imageStyle}
+      src={icon}
+      alt="img"
+    />
+    <p>{name}</p>
+    </div>
+    
+  );
+}
 
 export function Navigation(props) {
   return (
-    <div style={ {
+    <div style={{
       display: 'flex',
-      flexDirection: 'column'}}>
-      <p>
-      <button
-        onClick={() => props.activePageHandler("Calendar")}
-      >Calendar</button>
-      </p>
-      <p>
-      <button
-        onClick={() => props.activePageHandler("Courses")}
-      >Courses </button>
-      </p>
+      flexDirection: 'column',
+      gap: '20px',
+    }}>
+      <ImageWithStyle
+        pageHandler={() => props.activePageHandler("Calendar")}
+        icon={calendar_icon}
+        isActive={props.activePage === "Calendar"}
+        name={"Calendar"}
+      />
+      <ImageWithStyle
+        pageHandler={() => props.activePageHandler("Courses")}
+        icon={learning_icon}
+        isActive={props.activePage === "Courses"}
+        name={"Courses"}
+      />
     </div>
   );
 }
